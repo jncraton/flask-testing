d98fee32c57e5b762777103179bb38878668814e
Jon Craton <jncraton@gmail.com>
2025-09-23 12:38:57 -0400

    Add parameterized test

--- tests/test_app.py
+++ tests/test_app.py
@@ -14,3 +14,14 @@ def test_add(client):
     response = client.get("/add?a=5&b=6")
     assert b"11" in response.data
 
+addition = [
+    (1,1,2),
+    (1,2,3),
+    (0,0,0),
+    (-1,2,1),
+]
+@pytest.mark.parametrize("a,b,expected", addition)
+def test_add_parameterized(client, a, b, expected):
+    response = client.get(f"/add?a={a}&b={b}")
+    assert str(expected).encode() in response.data
+
